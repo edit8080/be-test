49532b09db291acb021a247616e597b48206445a
이태희 <leetaehee0205@gmail.com>
2021-12-21 00:45:27 +0900

    feat : init flask server

---
+++ main/app.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db = SQLAlchemy()
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+api = Api(app)
+
+if __name__ == '__main__':
+  db.init_app(app)
+  app.run(port=5000, debug=True)
