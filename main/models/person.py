c67d8ad98fb4868e61aa572fadb7b60f36c1c75c
이태희 <leetaehee0205@gmail.com>
2021-12-22 00:17:42 +0900

    feat(models): create filter function using gender, race, ethnicity

--- main/models/person.py
+++ main/models/person.py
@@ -14,9 +14,21 @@ class PersonModel(db.Model):
       'gender': self.gender_source_value,
       'birth': self.birth_datetime,
       'race': self.race_source_value,
-      'ethnicity_concept_id': self.ethnicity_source_value,
+      'ethnicity': self.ethnicity_source_value,
     }
 
   @classmethod
   def get_all_person(cls):
     return [person.json() for person in cls.query.all()]
+
+  @classmethod
+  def find_person_by_gender(cls, gender):
+    return [person.json() for person in cls.query.filter_by(gender_source_value=gender).all()]
+
+  @classmethod
+  def find_person_by_race(cls, race):
+    return [person.json() for person in cls.query.filter_by(race_source_value=race).all()]
+
+  @classmethod
+  def find_person_by_ethnicity(cls, ethnicity):
+    return [person.json() for person in cls.query.filter_by(ethnicity_source_value=ethnicity).all()]
