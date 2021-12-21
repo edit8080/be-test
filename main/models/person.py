faff1c30cc5a4258f07b995b3a7a831206b9c8ab
이태희 <leetaehee0205@gmail.com>
2021-12-21 23:52:50 +0900

    feat(models): get all person data and sequalize by json

--- main/models/person.py
+++ main/models/person.py
@@ -1,9 +1,22 @@
 from app import db
 
 class PersonModel(db.Model):
-  __tablename__ = "de.person"
+  __tablename__ = "person"
   person_id = db.Column(db.BigInteger, primary_key=True)
   gender_concept_id = db.Column(db.Integer, db.ForeignKey('de.concept.concept_id'))
-  birth_datetime = db.Column(db.datetime)
+  birth_datetime = db.Column(db.DateTime)
   race_concept_id = db.Column(db.Integer, db.ForeignKey('de.concept.concept_id'))
   ethnicity_concept_id = db.Column(db.Integer)
+
+  def json(self):
+    return {
+      'person_id': self.person_id,
+      'gender_concept_id': self.gender_concept_id,
+      'birth_datetime': self.birth_datetime,
+      'race_concept_id': self.race_concept_id,
+      'ethnicity_concept_id': self.ethnicity_concept_id,
+    }
+
+  @classmethod
+  def get_all_person(cls):
+    return [person.json() for person in cls.query.all()]
