79a6c3c325803c22ecfbfdb4c972a055f75a74b8
이태희 <leetaehee0205@gmail.com>
2021-12-25 19:21:26 +0900

    chore: import visit resources in app

--- main/app.py
+++ main/app.py
@@ -14,6 +14,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
 
 from resources.person import *
+from resources.visit import *
 
 if __name__ == '__main__':
   db.init_app(app)
