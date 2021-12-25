69db3df2caf9326113267d4737e1c013230c0c8c
이태희 <leetaehee0205@gmail.com>
2021-12-25 19:21:11 +0900

    feat(reasources): update visit api

--- main/resources/visit.py
+++ main/resources/visit.py
@@ -1,22 +1,25 @@
 from app import app
-from flask import request
+from models.visit import VisitModel
 
 @app.route('/visit/type/<string:type>', methods=['GET'])
 def visitByType(type): # 방문 유형별 방문 수
-  pass
+  return { 'count': len(VisitModel.find_visit_by_type(type)) }
 
 @app.route('/visit/gender/<string:gender>', methods=['GET'])
 def visitByGender(gender): # 성별 방문 수
-  pass
+  return { 'count': len(VisitModel.find_visit_by_gender(gender)) }
+
 
 @app.route('/visit/race/<string:race>', methods=['GET'])
 def visitByRace(race): # 인종별 방문 수
-  pass
+  return { 'count': len(VisitModel.find_visit_by_race(race)) }
 
 @app.route('/visit/ethnicity/<string:ethnicity>', methods=['GET'])
 def visitByEthnicity(ethnicity): # 민족별 방문 수
-  pass
+  return { 'count': len(VisitModel.find_visit_by_ethnicity(ethnicity)) }
 
-@app.route('/visit/age/<number:age>', methods=['GET'])
-def visitByAge(): # 방문시 연령대(10세 단위)별 방문 수
-  pass
+@app.route('/visit/age/<int:age_unit>', methods=['GET'])
+def visitByAge(age_unit): # 방문시 연령대(10세 단위)별 방문 수
+  # todo: age_unit 10세 단위 체크 
+  # todo: 잘못된 입력 체크
+  return { 'count': len(VisitModel.find_visit_by_age(age_unit)) }
