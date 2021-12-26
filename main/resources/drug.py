aa0de679c6f3213d519b6788baa79cec612ef449
이태희 <leetaehee0205@gmail.com>
2021-12-26 16:23:19 +0900

    feat(resources): adjust pagination in drug api resources

--- main/resources/drug.py
+++ main/resources/drug.py
@@ -7,14 +7,17 @@ def DrugByConcept():
   name = request.args.get('name')
   domain = request.args.get('domain')
 
+  page = request.args.get('page', type=int)
+  per_page = request.args.get('per_page', type=int)
+
   if name and domain:
-    return { 'concept': DrugModel.find_drug_concept_by_name_and_domain(name, domain) }
+    return DrugModel.find_drug_concept_by_name_and_domain(name, domain, page, per_page)
 
   elif name:
-    return { 'concept': DrugModel.find_drug_concept_by_name(name) }
+    return DrugModel.find_drug_concept_by_name(name, page, per_page)
 
   elif domain:
-    return { 'concept': DrugModel.find_drug_concept_by_domain(domain) }
+    return DrugModel.find_drug_concept_by_domain(domain, page, per_page)
 
   else:
     return { 'msg': 'use name or domain query string to search concept' }, 400
