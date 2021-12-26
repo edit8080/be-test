4737f8a3a6f92c1bfb880bcbeccf46207dafb9f1
이태희 <leetaehee0205@gmail.com>
2021-12-26 16:24:41 +0900

    chore(common): add comment about pagination method

--- main/common/cls/pagination.py
+++ main/common/cls/pagination.py
@@ -4,9 +4,10 @@ class Pagination:
     self.per_page = per_page
 
   def set_pagination(self, sqlQuery):
-    if (self.page or self.per_page):
+    if (self.page or self.per_page): # page, per_page 중 하나라도 명시하면 페이지네이션된 데이터
       results = sqlQuery.paginate(self.page or 1, self.per_page or 20)
       return { 'items': results.items, 'page': results.page, 'total': results.total }
-    else:
+
+    else: # page, per_page 를 명시하지 않으면 전체 데이터
       results = sqlQuery.all()
       return { 'items': results, 'page': 1, 'total': len(results) }
